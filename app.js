08177bf8c903df1a22177ec80674c685b33f1863
Vyttali <joaquiin.ok@gmail.com>
2025-08-15 19:04:11 -0300

    actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Crea una lista
+let amigos = [];
+
+//Funcion para agregar nombres a la lista
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert("Por favor, escribe un nombre");
+        return;
+    }
+
+
+}
+
+//Verifica si el nombre ya esta en la lista
+if (amigos.includes(nombre)) {
+    alert('Este nombre ya esta en la lista. Porfavor elige otro nombre diferente');
+    input.value = '';
+}
