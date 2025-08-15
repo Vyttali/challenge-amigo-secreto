417eb26358dcb096b5687f122ef0a17c665c00da
Vyttali <joaquiin.ok@gmail.com>
2025-08-15 19:23:04 -0300

    agrega nombre y limpia input

--- app.js
+++ app.js
@@ -2,21 +2,31 @@
 // Crea una lista
 let amigos = [];
 
-//Funcion para agregar nombres a la lista
+// Funcion para agregar nombres a la lista
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const nombre = input.value.trim();
 
     if (nombre === '') {
-        alert("Por favor, escribe un nombre");
+        alert("Por favor, escribe un nombre válido");
         return;
     }
 
+// Verificar que el nombre no se repita
+    if (amigos.includes(nombre)) {
+        alert('Este nombre ya esta en la lista, porfavor elegi otro');
+        input.value = '';
+        return;
+    }
 
-}
+// Agregar el nombre al array
+    amigos.push(nombre);
+    const lista = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    lista.appendChild(li)
 
-//Verifica si el nombre ya esta en la lista
-if (amigos.includes(nombre)) {
-    alert('Este nombre ya esta en la lista. Porfavor elige otro nombre diferente');
+// Limpiar el input
     input.value = '';
 }
+
